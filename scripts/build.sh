4fcd43bf756800564b8fefdfbe0f0892079927de
Nurlan Su <nurlansu@users.noreply.github.com>
2017-05-10 10:27:46 +0200

    Updated npm deps

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
+set -e
+
 # Building main stuff (npm)
 npm run build:ci
+
 # Building Hugo themes
 THEMES=(hugo-sustain hugo-vitae)
 for i in "${THEMES[@]}"
@@ -12,7 +15,9 @@ do
   # Building themes
   hugo -s ./source/themes/"$i"
 done
+
 # Building .html files
 cp ./source/*.html ./public/
+
 # Minifying html files
 minify -r -o ./public/ --match=\.html ./public/
